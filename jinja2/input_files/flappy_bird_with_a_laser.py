715d5d8e8f3fdf66a8d18dddc6c3bcb536fa8829
NC298197 <NC298197@falmouth.ac.uk>
2026-07-17 02:59:43 +0100

    different descriptions now on the flappy bird w/ laser page

--- jinja2/input_files/flappy_bird_with_a_laser.py
+++ jinja2/input_files/flappy_bird_with_a_laser.py
@@ -47,14 +47,8 @@ project_data = ProjectSiteData(
         BodyContent("Original Web Version", "November 14, 2023",
 
             r"""
-            <b>Flappy bird with a twist! Use a second button to zap enemies out of the air, while avoiding the robo-pipes coming your way.</b>
-            <br><br>
-             For our portfolio project, my aim was to build two out of three mobile apps and make a website over the course of seven weeks. For my third project, I decided I would save the
-             concepting and port one of my older projects to mobile, and this was the natural choice. 
-            <br><br>
-            This did bring some design problems though - wanting to recreate the portrait mode from the original, the missile enemies from the original were impossible to predict, and
-            the art style was far too noisy for such a small resolution. So in order to keep the enemies on the screen a bit longer, I decided to utilise DOTween to have them enter the
-            screen, hang around a bit, before attaching to the background and accelerating forward, creating thematic "Hawk" sprites to match.
+            Flappy bird with a laser was originally conceived as a response to our "Modding Brief" at Falmouth University, but upon playing their Flappy Bird starter project (adding
+            momentum when you jump, really?), I figured that the only thing to do was to recreate Flappy Bird in Unity from scratch :).
             """
         )
     ]
