2c9665a0e8cca3bccaa926ba6cdcc5f20a47c9f1
NC298197 <NC298197@falmouth.ac.uk>
2026-07-20 09:08:29 +0100

    added chaos-balls, and spacefight to main menu

---
+++ jinja2/input_files/chaos_balls.py
@@ -0,0 +1,41 @@
+from project_site_data import ProjectSiteData, Link, GalleryImage, BodyContent
+
+
+project_data = ProjectSiteData(
+    
+    title = "Chaos Balls",
+
+
+    role = "Designer, Programmer, Artist",
+
+    thumbType = "png",
+
+    imgRendering = "pixelated",
+
+    galleryImages = [
+        GalleryImage("screenshot1.png", "Gameplay")
+    ],
+
+
+    links = [
+        Link("Chaos Balls on Itch.io", "https://nedinyourhead.itch.io/chaos-balls")
+    ],
+
+
+    bodyContent = [
+        
+        BodyContent(r"<i>It's a Winter Wonder-Jam!</i>", r"Month DD, YYYY",
+
+            r"""
+            <b>Organise as many balls as possible in 60 seconds by whacking them into the goals using your baseball bat- but be careful, as getting hit by one will lead to an instant
+            game-over!</b>
+            <br><br>
+            A game made in 6 days for GavinCraft's It's a Winter Wonder-Jam! with the prompt: "Organised-Chaos", as part of a series of jams in which I attempted to learn Godot. This
+            was the last of three, and although she's not a looker, the outcome was more than fun enough for me to consider the attempt a success.
+            <br><br>
+            I hope you'll have as much fun as I did with it!
+            """
+
+        )
+    ]
+)
